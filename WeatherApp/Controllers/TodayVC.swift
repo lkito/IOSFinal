03885cf0a05624c566ec444f93f704b200b2c782
Kt <lkiti17@freeuni.edu.ge>
2022-02-18 19:41:12 -0800

    Adds blur

--- WeatherApp/Controllers/TodayVC.swift
+++ WeatherApp/Controllers/TodayVC.swift
@@ -19,10 +19,14 @@ class TodayVC: UIViewController {
     @IBOutlet var locationLabel: UILabel!
     @IBOutlet var weatherLabel: UILabel!
     
+    @IBOutlet var loader: UIActivityIndicatorView!
+    @IBOutlet var blur: UIVisualEffectView!
+    
     private let weatherService = WeatherService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        blur.isHidden = true
         cloudDetail.setIcon(iconName: "raining.pdf")
         humidityDetail.setIcon(iconName: "drop.pdf")
         pressureDetail.setIcon(iconName: "celsius.pdf")
@@ -30,19 +34,61 @@ class TodayVC: UIViewController {
         windDirectionDetail.setIcon(iconName: "compass.pdf")
         
         loadTodayWeatherData()
-        weatherService.getWeatherImage(imageName: "02n") { result in
-            switch result {
-                case .success(let weather):
-                    print(weather)
-                case .failure(let error):
-                    print(error)
-            }
-
+    }
+    
+    private func visualizeWeatherDetails(weather: CurrentWeatherResponse, strongSelf: TodayVC) {
+        if weather.weather.count == 0 {
+            strongSelf.blur.isHidden = true
+            strongSelf.loader.stopAnimating()
+            return
         }
+        let firstWeather = weather.weather[0]
+        strongSelf.weatherLabel.text = String(Int(weather.main.temp)) + "°C | " + firstWeather.main
+        strongSelf.cloudDetail.setDetail(detailValue: String(weather.clouds.all) + " %")
+        strongSelf.humidityDetail.setDetail(detailValue: String(weather.main.humidity) + " mm")
+        strongSelf.pressureDetail.setDetail(detailValue: String(weather.main.pressure) + " hPa")
+        strongSelf.windVelocityDetail.setDetail(detailValue: String(weather.wind.speed) + " km/h")
+        strongSelf.windDirectionDetail.setDetail(detailValue: LocationService.shared.degreesToDirection(degrees: weather.wind.deg))
+        
+        strongSelf.weatherService.getWeatherImage(imageName: firstWeather.icon, completion: { [weak self] result in
+            guard let strongSelf = self else {
+                return
+            }
+            DispatchQueue.main.async {
+                switch result {
+                    case .success(let image):
+                        strongSelf.weatherImage.image = image
+                    case .failure(let error):
+                        print(error)
+                }
+                strongSelf.blur.isHidden = true
+                strongSelf.loader.stopAnimating()
+            }
+        })
     }
 
+    private func loadWeatherDetails(location: CLLocation, strongSelf: TodayVC) {
+        strongSelf.weatherService.getCurrentWeather(lat: location.coordinate.latitude,
+                                                    lon: location.coordinate.longitude) { [weak self] result in
+            DispatchQueue.main.async {
+                guard let strongSelf = self else {
+                    return
+                }
+                switch result {
+                    case .success(let weather):
+                        strongSelf.visualizeWeatherDetails(weather: weather, strongSelf: strongSelf)
+                    case .failure(let error):
+                        strongSelf.blur.isHidden = true
+                        strongSelf.loader.stopAnimating()
+                        print(error)
+                }
+            }
+        }
+    }
     
     private func loadTodayWeatherData() {
+        loader.startAnimating()
+        blur.isHidden = false
         LocationService.shared.getUserLocation(completion: { [weak self] location in
             guard let strongSelf = self else {
                 return
@@ -54,53 +100,15 @@ class TodayVC: UIViewController {
                     }
                     switch result {
                         case .success(let locationName):
-                            print(locationName)
                             strongSelf.locationLabel.text = locationName
-                            strongSelf.locationLabel.sizeToFit()
                         case .failure(let error):
+                            strongSelf.blur.isHidden = true
+                            strongSelf.loader.stopAnimating()
                             print(error)
                     }
                 }
             })
-            strongSelf.weatherService.getCurrentWeather(lat: location.coordinate.latitude,
-                                                        lon: location.coordinate.longitude) { [weak self] result in
-                DispatchQueue.main.async {
-                    guard let strongSelf = self else {
-                        return
-                    }
-                    switch result {
-                        case .success(let weather):
-                            print(weather)
-                            if weather.weather.count == 0 {
-                                return
-                            }
-                            let firstWeather = weather.weather[0]
-                            strongSelf.weatherLabel.text = String(Int(weather.main.temp)) + "°C | " + firstWeather.main
-                            strongSelf.cloudDetail.setDetail(detailValue: String(weather.clouds.all) + " %")
-                            strongSelf.humidityDetail.setDetail(detailValue: String(weather.main.humidity) + " mm")
-                            strongSelf.pressureDetail.setDetail(detailValue: String(weather.main.pressure) + " hPa")
-                            strongSelf.windVelocityDetail.setDetail(detailValue: String(weather.wind.speed) + " km/h")
-                            strongSelf.windDirectionDetail.setDetail(detailValue: LocationService.shared.degreesToDirection(degrees: weather.wind.deg))
-                            
-                            strongSelf.weatherService.getWeatherImage(imageName: firstWeather.icon, completion: { [weak self] result in
-                                guard let strongSelf = self else {
-                                    return
-                                }
-                                DispatchQueue.main.async {
-                                    switch result {
-                                        case .success(let image):
-                                            strongSelf.weatherImage.image = image
-                                        case .failure(let error):
-                                            print(error)
-                                    }
-                                }
-                            })
-                            
-                        case .failure(let error):
-                            print(error)
-                    }
-                }
-            }
+            strongSelf.loadWeatherDetails(location: location, strongSelf: strongSelf)
         })
     }
 
